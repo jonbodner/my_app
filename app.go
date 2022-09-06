7d61e55818ee5002e5c38123501aef5631e37365
Jon Bodner <jon@bodnerfamily.com>
2022-09-05 21:29:45 -0400

    first release

---
+++ app.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jonbodner/my_lib"
+)
+
+func main() {
+	fmt.Println(my_lib.AddNums(2, 3))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jonbodner/my_app
 
 go 1.19
+
+require github.com/jonbodner/my_lib v1.0.0
